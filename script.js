27c101b1b5f45a7fa3b32e49a89d97b6d083c89c
Sukhanpreet <sukhanpreet356@gmail.com>
2022-07-19 09:15:01 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,83 @@
+let count=0;
+
+let note=document.getElementById('note');
+
+let add_button=document.getElementById('add-note');
+
+let notesArea=document.getElementById('notes');
+
+add_button.addEventListener('click',addTask);
+
+function addTask(){
+    if(note.value!==""){
+        count +=1;
+        // console.log(count);
+        let newDiv= document.createElement('div');
+
+        notesArea.append(newDiv);
+        newDiv.setAttribute('class','note-content');
+
+
+        let noteHeading=document.createElement('h3');
+        let noteText=document.createElement('p');
+        let viewMoreButton=document.createElement('input');
+        let deleteButton=document.createElement('input');
+
+        noteHeading.innerText="Note "+count;
+        noteText.innerText=note.value;
+        
+        Object.assign(viewMoreButton,{
+            type:'button',
+            value:'View More'
+        });
+
+        Object.assign(deleteButton,{
+            type:'button',
+            id: 'delete-button',
+            value: 'x'
+        });
+
+        viewMoreButton.addEventListener('click',showModel);
+        deleteButton.addEventListener('click',deleteNote);
+
+        newDiv.append(noteHeading);
+        newDiv.append(noteText);
+        newDiv.append(viewMoreButton);
+        newDiv.append(deleteButton);
+        
+        note.value='';
+
+    } else{
+        // console.log(count);
+        alert("Please write something");
+    }
+
+}
+
+let modelContainer=document.querySelector('.model-container');
+
+function showModel(){
+   
+    modelContainer.setAttribute('style','display:block;');
+
+    let model=modelContainer.querySelector('p');
+
+    model.innerText=this.previousElementSibling.innerText;
+    
+    let closeButton=modelContainer.querySelector('input');
+
+    closeButton.addEventListener('click', function(){
+        modelContainer.setAttribute('style','display:none;');
+    });
+
+    window.addEventListener('click',function(e){
+        if(e.target === modelContainer){
+            modelContainer.setAttribute('style','display:none;');
+        }
+    });
+
+}
+
+function deleteNote(){
+    this.parentElement.remove();
+}
